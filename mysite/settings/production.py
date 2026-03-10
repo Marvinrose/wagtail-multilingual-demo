5f2b5355b2a5e1687d5462ad19e65dc5e3ae16ae
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2026-03-10 02:38:19 +0100

    Fix production settings to read SECRET_KEY from env

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,21 +1,31 @@
+import os
 from .base import *
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com']
 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-insecure-key')
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
-# ManifestStaticFilesStorage is recommended in production, to prevent
-# outdated JavaScript / CSS assets being served from cache
-# (e.g. after a Wagtail upgrade).
-# See https://docs.djangoproject.com/en/6.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'},
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
     },
 }
